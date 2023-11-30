7b1cadc69fd3c724ef5475573a17a5762cc69afa
Yasser Samir <yassersamir.94@hotmail.com>
2023-11-30 04:02:45 -0500

    taaassskkk333

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,7 +1,7 @@
 import random
 
 number = random.randint(-10000, 10000)
-last_digit = number % 10
+last_digit = abs(number) % 10  # Use abs to get the absolute value of the number
 
 print(f"Last digit of {number} is {last_digit if number >= 0 else -last_digit} and", end=" ")
 
