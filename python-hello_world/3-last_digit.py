58dc7b601efd440668d1bacb56d1270128f500d2
Yasser Samir <yassersamir.94@hotmail.com>
2023-11-30 02:41:10 -0500

    task3 add negative last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,7 +2,7 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 
-print(f"Last digit of {number} is {last_digit} and", end=" ")
+print(f"Last digit of {number} is {last_digit if number >= 0 else -last_digit} and", end=" ")
 
 if last_digit > 5:
     print("is greater than 5")
