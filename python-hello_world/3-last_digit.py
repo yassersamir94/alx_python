95b6dfc7f3c85e73746bbbabb6829bef27394c59
Yasser Samir <yassersamir.94@hotmail.com>
2023-11-30 03:59:19 -0500

    task3 try to solve -ve last digit value issue

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -10,5 +10,5 @@ if last_digit > 5:
 elif last_digit == 0:
     print("is 0")
 else:
-    print("is less than 6 and not 0")
+    print("is less than 6 and not 0" if number >= 0 else "is less than 6 and not 0")
     
