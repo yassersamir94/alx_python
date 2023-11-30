f9951411027051313687a016a1f5eba6e8a92860
Yasser Samir <yassersamir.94@hotmail.com>
2023-11-30 03:14:38 -0500

    again and again task3

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -8,10 +8,10 @@ last_digit = number % 10
 print(f"Last digit of {number} is {last_digit if number >= 0 else -last_digit} and", end=" ")
 
 # Check conditions and print the corresponding message
-if abs(last_digit) > 5:
-    print("is greater than 5")
+if number < 0 and abs(last_digit) > 5:
+    print("is less than 6 and not 0 and is negative")
 elif abs(last_digit) == 0:
     print("is 0")
 else:
-    print(f"is less than 6 and not 0")
+    print(f"is less than 6 and not 0 and is greater than -5")
     
