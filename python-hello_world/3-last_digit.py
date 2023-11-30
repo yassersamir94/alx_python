2f9244062268d85e311587256e5c4107754fec0e
Yasser Samir <yassersamir.94@hotmail.com>
2023-11-30 03:56:13 -0500

    task 3 never end

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -9,5 +9,6 @@ if last_digit > 5:
     print("is greater than 5")
 elif last_digit == 0:
     print("is 0")
-elif last_digit < 6 and last_digit != 0 and number < 0:
+else:
     print("is less than 6 and not 0")
+    
