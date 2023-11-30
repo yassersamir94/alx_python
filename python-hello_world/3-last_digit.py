421f4a5e93cdd2779e934da2bea369f3046e683d
Yasser Samir <yassersamir.94@hotmail.com>
2023-11-30 03:26:45 -0500

    try task 3 to solve negative last digit error

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,6 +1,6 @@
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
+last_digit = number % 10
 
 print(f"Last digit of {number} is {last_digit if number >= 0 else -last_digit} and", end=" ")
 
@@ -8,5 +8,5 @@ if last_digit > 5:
     print("is greater than 5")
 if last_digit == 0:
     print("is 0")
-if abs(last_digit) <6 and last_digit !=0:
+if last_digit <6 and last_digit !=0 and last_digit <0:
     print("is less than 6 and not 0")
