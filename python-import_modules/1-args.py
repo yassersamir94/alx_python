94b282a1965eaac6ef87f8828e9f05b26dd65693
Yasser Samir <yassersamir.94@hotmail.com>
2023-12-06 01:10:18 -0500

    Task1

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,13 +1,17 @@
 from sys import argv
 
-num_args = len(argv) - 1
+def print_arguments():
+    num_args = len(argv) - 1
 
-if num_args == 0:
-    print("0 arguments.")
-elif num_args == 1:
-    print("1 argument:")
-    print("1: {}".format(argv[1]))
-else:
-    print("{} arguments:".format(num_args))
-    for i in range(1, num_args + 1):
-        print("{}: {}".format(i, argv[i]))
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(num_args))
+        for i in range(1, num_args + 1):
+            print("{}: {}".format(i, argv[i]))
+
+if __name__ == "__main__":
+    print_arguments()
