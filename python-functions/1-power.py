877d310b2514b68fbc6e542e569441354e9f7ee5
Yasser Samir <yassersamir.94@hotmail.com>
2023-12-06 00:06:01 -0500

    functions task1

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,2 +1,16 @@
 def pow(a, b):
-    return a ^ b
+    if b == 0:
+        return 1
+    neg_exponent = False
+    
+    if b < 0:
+        neg_exponent = True
+        b = -b
+    result = 1
+    
+    for _ in range(b):
+        result *= a
+        
+    if neg_exponent:
+        result = 1 / result
+    return result
