76b6998f7e13a36e24f83db9d9aa1d98a5e8db15
Yasser Samir <yassersamir.94@hotmail.com>
2023-12-06 01:07:42 -0500

    Task1

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+num_args = len(argv) - 1
+
+if num_args == 0:
+    print("0 arguments.")
+elif num_args == 1:
+    print("1 argument:")
+    print("1: {}".format(argv[1]))
+else:
+    print("{} arguments:".format(num_args))
+    for i in range(1, num_args + 1):
+        print("{}: {}".format(i, argv[i]))
