1f9e324db924d9115dc46c909ddacf15997e5b75
Yasser Samir <yassersamir.94@hotmail.com>
2024-02-01 17:35:38 -0500

    task0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,1 +1,46 @@
+guillaume@ubuntu:~/$ cat 1-main.py
+#!/usr/bin/python3
+Square = __import__('1-square').Square
 
+my_square_1 = Square(3)
+print(type(my_square_1))
+print(my_square_1.__dict__)
+
+my_square_2 = Square()
+print(type(my_square_2))
+print(my_square_2.__dict__)
+
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_3 = Square("3")
+    print(type(my_square_3))
+    print(my_square_3.__dict__)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_4 = Square(-89)
+    print(type(my_square_4))
+    print(my_square_4.__dict__)
+except Exception as e:
+    print(e)
+
+guillaume@ubuntu:~/$ python3 1-main.py
+<class '1-square.Square'>
+{'_Square__size': 3}
+<class '1-square.Square'>
+{'_Square__size': 0}
+'Square' object has no attribute 'size'
+'Square' object has no attribute '__size'
+size must be an integer
+size must be >= 0
+guillaume@ubuntu:~/$
