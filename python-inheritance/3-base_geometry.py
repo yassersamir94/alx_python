022f69d8660a8dce02b86dc649d0db729a9fa19c
Yasser Samir <yassersamir.94@hotmail.com>
2024-02-01 18:24:22 -0500

    task3

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,12 @@
+"""
+3-base_geometry.py: Module containing an empty class representing base geometry.
+"""
+
+class BaseGeometry:
+    """
+    Empty class representing base geometry.
+
+    This class serves as a base for other geometry-related classes.
+    It does not contain any methods or attributes.
+    """
+    pass
