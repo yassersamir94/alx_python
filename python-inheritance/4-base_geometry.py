6135ccfb78cbe444ba3848e65528422386060745
Yasser Samir <yassersamir.94@hotmail.com>
2024-02-05 18:42:53 -0500

    task 4

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,17 +1,15 @@
 """
-4-base_geometry.py: Module containing the BaseGeometry class with an area() method.
+4-base_geometry.py - Module containing the BaseGeometry class with an area() method.
 """
 
 class BaseGeometry:
     """
-    Base class representing geometry.
-
-    This class serves as a foundation for other geometry-related classes.
+    BaseGeometry class representing basic geometry operations.
     """
 
     def area(self):
         """
-        Compute the area of the geometry.
+        Computes the area of the geometry.
 
         Raises:
             Exception: Indicates that the area() method is not implemented.
