883588772c8002ba50f465c41018bf4aeb15113b
Yasser Samir <yassersamir.94@hotmail.com>
2024-02-06 17:38:30 -0500

    44444

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -17,3 +17,25 @@ html_content = """
 
 with open("output.html", "w") as f:
     f.write(html_content)
+
+class BaseGeometry:
+    """
+    BaseGeometry class represents a base class for geometry operations.
+
+    This class provides a method to raise an exception when the area calculation is not implemented.
+    """
+
+    def area(self):
+        """
+        Calculate the area of the geometry.
+
+        Raises:
+            Exception: Indicates that the area calculation is not implemented.
+        """
+        raise Exception("area() is not implemented")
+
+    def __init_subclass__(cls):
+        """
+        Empty method to prevent implicit addition of __init_subclass__.
+        """
+        pass
