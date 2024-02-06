d1c55ebc8f71dcc1c8cfe04c77809d67c4edc8d8
Yasser Samir <yassersamir.94@hotmail.com>
2024-02-06 16:52:53 -0500

    4444

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-"""
-Module for BaseGeometry class.
-
-This module defines the BaseGeometry class, which serves as a base class
-for geometry-related operations. It currently provides a method to raise
-an exception when the area calculation is not implemented.
-"""
+"""Module for BaseGeometry class"""
 
 
 class BaseGeometry:
@@ -14,13 +8,3 @@ class BaseGeometry:
     def area(self):
         """Raises an Exception with the message area() is not implemented"""
         raise Exception("area() is not implemented")
-
-    def __init_subclass__(cls):
-         """
-        Empty method to override implicit addition of __init_subclass__.
-
-        This method is empty to prevent Python from adding __init_subclass__
-        implicitly to the class. It ensures that the output of dir(bg) does not
-        include __init_subclass__.
-        """
-        pass
