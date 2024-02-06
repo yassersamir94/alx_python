3f7610f90266805119c3536bc932026181a76a11
Yasser Samir <yassersamir.94@hotmail.com>
2024-02-06 00:06:31 -0500

    task5

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -92,3 +92,8 @@ class Rectangle(Base):
         """Prints the rectangle with '#' characters."""
         for _ in range(self.height):
             print('#' * self.width)
+    def __str__(self):
+        """Returns a string representation of the Rectangle instance."""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height
+        )
