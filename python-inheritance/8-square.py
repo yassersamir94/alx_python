853bedc81168b5659d7141fef7e10cdc79e43582
Yasser Samir <yassersamir.94@hotmail.com>
2024-02-05 19:09:02 -0500

    task88

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -3,18 +3,15 @@
 Module 8-square.py - Defines the Square class.
 """
 
-from typing import Union
-Rectangle = __import__('7-rectangle').Rectangle
-
-class Square(Rectangle):
+class Square:
     """
-    Square class that inherits from Rectangle.
+    Square class that represents a square shape.
 
     Attributes:
         __size (int): The size of the square.
     """
 
-    def __init__(self, size: int):
+    def __init__(self, size):
         """
         Initializes a Square instance with a given size.
 
@@ -23,9 +20,8 @@ class Square(Rectangle):
         """
         self.__size = size
         self.integer_validator("size", size)
-        super().__init__(size, size)
 
-    def __str__(self) -> str:
+    def __str__(self):
         """
         Returns a string representation of the Square object.
 
@@ -34,11 +30,28 @@ class Square(Rectangle):
         """
         return "[Rectangle] {}/{}".format(self.__size, self.__size)
 
-    def area(self) -> Union[int, float]:
+    def area(self):
         """
         Computes the area of the Square.
 
         Returns:
-            Union[int, float]: The area of the Square.
+            int: The area of the Square.
         """
         return self.__size * self.__size
+
+    def integer_validator(self, name, value):
+        """
+        Validates the value to be an integer and greater than 0.
+
+        Args:
+            name (str): The name of the value being validated.
+            value (int): The value to be validated.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
