3ab841d90cd0da67336d8a7f1fd6a3fa9f663422
Yasser Samir <yassersamir.94@hotmail.com>
2024-02-06 16:58:07 -0500

    4444

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -8,14 +8,3 @@ class BaseGeometry:
     def area(self):
         """Raises an Exception with the message area() is not implemented"""
         raise Exception("area() is not implemented")
-    
-    @classmethod
-    def __init_subclass__(cls):
-        """
-        Empty method to override implicit addition of __init_subclass__.
-
-        This method is empty to prevent Python from adding __init_subclass__
-        implicitly to the class. It ensures that the output of dir(bg) does not
-        include __init_subclass__.
-        """
-        pass
