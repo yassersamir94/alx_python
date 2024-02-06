43106af2e285fb3a9eb296acacc74039cb03a525
Yasser Samir <yassersamir.94@hotmail.com>
2024-02-06 17:04:53 -0500

    444444

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -8,3 +8,13 @@ class BaseGeometry:
     def area(self):
         """Raises an Exception with the message area() is not implemented"""
         raise Exception("area() is not implemented")
+
+    def __init_subclass__(cls):
+        """Empty method to prevent implicit addition of __init_subclass__"""
+        pass
+
+if __name__ == "__main__":
+    # Code to execute when the script is run directly
+    print("Running 4-base_geometry.py")
+    bg = BaseGeometry()
+    print(dir(bg))
