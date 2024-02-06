d87da78b34c6b7b3eb691b5e865962b8d0ef18aa
Yasser Samir <yassersamir.94@hotmail.com>
2024-02-06 17:06:15 -0500

    3333333

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-"""
-3-base_geometry.py: Module containing an empty class representing base geometry.
-"""
+"""Module for BaseGeometry class"""
+
 
 class BaseGeometry:
-    """
-    This module defines the BaseGeometry class.
-    """
+    """Empty class BaseGeometry"""
 
-    pass
+    def area(self):
+        """Raises an Exception with the message area() is not implemented"""
+        raise Exception("area() is not implemented")
+
+if __name__ == "__main__":
+    # Code to execute when the script is run directly
+    print("Running 3-base_geometry.py")
+    bg = BaseGeometry()
+    print(dir(bg))
