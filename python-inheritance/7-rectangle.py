00b0e181ee58b7aa7287ae8e77e900482fdbd1ed
Yasser Samir <yassersamir.94@hotmail.com>
2024-02-05 19:02:48 -0500

    task7

---
+++ python-inheritance/7-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+7-rectangle.py - Module for the Rectangle class.
+"""
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class that inherits from BaseGeometry.
+
+    Attributes:
+        __width (int): The width of the rectangle.
+        __height (int): The height of the rectangle.
+    """
+
+    def __init__(self, width, height):
+        """
+        Initializes a Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        super().__init__()
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Calculates the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Returns a string representation of the Rectangle.
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    @property
+    def width(self):
+        """
+        Retrieves the width of the Rectangle.
+
+        Returns:
+            int: The width of the Rectangle.
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """
+        Retrieves the height of the Rectangle.
+
+        Returns:
+            int: The height of the Rectangle.
+        """
+        return self.__height
