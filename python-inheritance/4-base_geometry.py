3240a232ce045bdbfaaa4214f089e18b0f445a75
Yasser Samir <yassersamir.94@hotmail.com>
2024-02-06 16:48:16 -0500

    44

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python3
-"""Module for BaseGeometry class"""
-
-
 class BaseGeometry:
     """Empty class BaseGeometry"""
 
     def area(self):
         """Raises an Exception with the message area() is not implemented"""
         raise Exception("area() is not implemented")
+
+    def __init_subclass__(cls):
+        pass
