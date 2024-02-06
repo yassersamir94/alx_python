7a3fc67f7c088c9c919102881031f6088aee969a
Yasser Samir <yassersamir.94@hotmail.com>
2024-02-06 17:39:52 -0500

    4

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,23 +1,6 @@
 #!/usr/bin/python3
 """Module for BaseGeometry class"""
 
-html_content = """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1 style="text-transform: uppercase;">Title</h1>
-</body>
-</html>
-"""
-
-with open("output.html", "w") as f:
-    f.write(html_content)
-
 class BaseGeometry:
     """
     BaseGeometry class represents a base class for geometry operations.
