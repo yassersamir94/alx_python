c341cb03b6790c0fc572a19b7631865c7f5401dd
Yasser Samir <yassersamir.94@hotmail.com>
2024-02-06 01:24:27 -0500

    task4

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""This module defines the BaseGeometry class."""
+
 class BaseGeometry:
+    """A class representing geometric shapes."""
+
     def area(self):
+        """Compute the area of the geometric shape."""
         raise Exception("area() is not implemented")
