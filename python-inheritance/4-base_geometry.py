1bee52b4ea1544c7a24d8979005ca81312ce5d06
Yasser Samir <yassersamir.94@hotmail.com>
2024-02-06 01:23:29 -0500

    task4

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,30 +1,4 @@
 #!/usr/bin/python3
-"""
-Module 4-base_geometry.py - Defines the BaseGeometry class.
-"""
-
-
 class BaseGeometry:
-    """
-    BaseGeometry class defines geometric operations.
-    """
-
     def area(self):
-        """
-        Computes the area of the geometry.
-
-        Raises:
-            Exception: Always raises an exception with the message "area() is not implemented".
-        """
         raise Exception("area() is not implemented")
-
-    def __dir__(self):
-        """
-        Customizes the dir() function to include specific attributes and default inherited methods.
-
-        Returns:
-            list: List of attribute names.
-        """
-        default_attributes = dir(super())
-        custom_attributes = ['area']  # Additional custom attributes
-        return sorted(default_attributes + custom_attributes)
