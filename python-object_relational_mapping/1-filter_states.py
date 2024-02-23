0d0b9bacd8850e050aa39a7edc50217ade950695
Yasser Samir <yassersamir.94@hotmail.com>
2024-02-23 17:42:07 -0500

    1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # Connect to MySQL database
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    # Create a cursor object using cursor() method
+    cur = conn.cursor()
+
+    # Execute a SQL query
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+
+    # Fetch all the rows using fetchall() method
+    rows = cur.fetchall()
+
+    # Print the result
+    for row in rows:
+        print(row)
+
+    # Close cursor and database connection
+    cur.close()
+    conn.close()
