3faf4219a1624ca6b0f20513651b1b8b5e7dbe9c
Yasser Samir <yassersamir.94@hotmail.com>
2024-03-09 01:48:20 -0500

    5

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -28,8 +28,7 @@ def is_number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return render_template('5-number.html', n=n)
-
+    return render_template('5-number.html', number=n)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
