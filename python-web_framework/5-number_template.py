f147f70cc2b0621b2463baad8253ec4b9b4e891a
Yasser Samir <yassersamir.94@hotmail.com>
2024-03-09 01:59:13 -0500

    5

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -1,39 +1,49 @@
 #!/usr/bin/python3
-'''Importing Flask form flask'''
+"""Importing Flask from flask"""
 from flask import Flask, render_template
-from markupsafe import escape
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Display 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
+def display_hbnb():
+    """Display 'HBNB'"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    return 'C {}'.format((text).replace("_", " "))
+def display_c(text):
+    """Display 'C ' followed by the value of the text variable
+    (replace underscore _ symbols with a space)
+    """
+    return 'C {}'.format(text.replace('_', ' '))
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def display_python(text):
-    return 'Python {}'.format(escape(text).replace('_', ' '))
+    """Display 'Python ' followed by the value of the text variable
+    (replace underscore _ symbols with a space)
+    """
+    return 'Python {}'.format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def is_number(n):
-    return '{} is a number'.format(escape(n))
+    """Display '<n> is a number' only if n is an integer"""
+    return '{} is a number'.format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
+    """Display a HTML page only if n is an integer: H1 tag: 'Number: n'
+    """
     return render_template('5-number.html', number=n)
 
 
