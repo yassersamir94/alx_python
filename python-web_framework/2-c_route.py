44fd85488734bca179f78e954eeceb3eefc34b67
Yasser Samir <yassersamir.94@hotmail.com>
2024-03-09 01:27:10 -0500

    2

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -1,30 +1,22 @@
 #!/usr/bin/python3
-"""
-Script that starts a Flask web application.
-"""
-
-from flask import Flask, escape
+'''Importing Flask form flask'''
+from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return 'Hello HBNB!'
-
+    return "Hello HBNB!"
 
 @app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    """Displays 'HBNB'"""
-    return 'HBNB'
-
+def hbnb():
+    return "HBNB"
 
 @app.route('/c/<text>', strict_slashes=False)
-def display_c(text):
-    """Displays 'C' followed by the value of the text variable."""
-    return 'C {}'.format(escape(text).replace('_', ' '))
+def c(text):
+    return 'C {}'.format((text).replace("_", " "))
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
