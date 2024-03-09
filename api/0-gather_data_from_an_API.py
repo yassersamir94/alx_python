6e9a3b1a0ecfabadcd360395a9b101040ea852dd
Yasser Samir <yassersamir.94@hotmail.com>
2024-03-09 03:07:56 -0500

    0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+import requests
+import sys
+
+id = sys.argv[1]
+
+request_user = requests.get('https://jsonplaceholder.typicode.com/users/'+id)
+request_todos = requests.get('https://jsonplaceholder.typicode.com/users/'+id+'/todos')
+
+data_user = request_user.json()
+data_todos = request_todos.json()
+
+completed = 0
+
+for i in data_todos:
+    if i.get('completed')==True:
+        completed = completed + 1
+
+print ('Employee {} is done with tasks({}/{}):'.format(data_user.get('name'), completed,len(data_todos)))
+
+for item in data_todos:
+    if item.get('completed') == True:
+        print('\t ' + item.get('title'))
---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import csv
+import requests
+import sys
+
+user_id = str(sys.argv[1])
+
+request_user = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+request_todos = "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
+
+data_user = requests.get(request_user).json()
+data_todos = requests.get(request_todos).json()
+
+filename = f"{user_id}.csv"
+
+with open(filename, "w", newline="") as file:
+    csvwriter = csv.writer(file, quoting=csv.QUOTE_ALL)
+    for task in data_todos:
+        csvwriter.writerow(
+            [user_id, str(data_user["username"]), task["completed"], task["title"]]
+        )
