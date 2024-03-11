8bd15bd412fdbea698e322ea28af9f36c7344a9f
Yasser Samir <yassersamir.94@hotmail.com>
2024-03-11 02:22:37 -0400

    0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,51 +1,35 @@
+#!/usr/bin/python3
+"""
+Checks student output for returning info from REST API
+"""
+
 import requests
 import sys
 
-def fetch_todo_list(employee_id):
-    """
-    Fetches the TODO list progress for a given employee ID from a REST API.
+users_url = "https://jsonplaceholder.typicode.com/users"
+todos_url = "https://jsonplaceholder.typicode.com/todos"
 
-    Args:
-        employee_id (int): The ID of the employee.
 
-    Returns:
-        None
-    """
-    # URL for the employee's TODO list
-    todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+def check_tasks(id):
+    """ Fetch user name, number of tasks """
 
-    try:
-        # Fetch data from the API
-        response = requests.get(todo_url)
-        todo_data = response.json()
+    resp = requests.get(todos_url).json()
 
-        # Get employee name
-        employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-        response = requests.get(employee_url)
-        employee_data = response.json()
-        employee_name = employee_data.get('name')
+    filename = 'student_output'
+    count = 0
+    with open(filename, 'r') as f:
+        next(f)
+        for line in f:
+            count += 1
+            # Check if the line starts with a tab followed by a space and ends with a newline
+            if line.startswith('\t ') and line.endswith('\n'):
+                print("Task {} Formatting: OK".format(count))
+            else:
+                print("Task {} Formatting: Incorrect".format(count))
 
-        # Count the number of completed tasks
-        completed_tasks = [task for task in todo_data if task['completed']]
-        num_completed_tasks = len(completed_tasks)
-        total_tasks = len(todo_data)
-
-        # Display the employee's TODO list progress
-        print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
-        for task in completed_tasks:
-            print(f"    {task['title']}")
-
-    except Exception as e:
-        print(f"Error: {e}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
+        print("Usage: python3 script.py <student_id>")
         sys.exit(1)
-
-    try:
-        employee_id = int(sys.argv[1])
-        fetch_todo_list(employee_id)
-    except ValueError:
-        print("Error: Employee ID must be an integer.")
-        sys.exit(1)
+    check_tasks(int(sys.argv[1]))
