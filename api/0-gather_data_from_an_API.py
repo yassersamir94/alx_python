e53a181163160805c9d0d30a30fa9e6810e12b0c
Yasser Samir <yassersamir.94@hotmail.com>
2024-03-11 03:57:11 -0400

    0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,32 +1,39 @@
 #!/usr/bin/python3
 """
-Script that, using a REST API, for a given employee ID, returns information about his/her TODO list progress.
+Script that, using a REST API, for a given employee ID,
+returns information about his/her TODO list progress.
 """
 
-import sys
 import requests
+from sys import argv
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} employee_id".format(sys.argv[0]))
-        sys.exit(1)
+    if len(argv) != 2:
+        print("Usage: {} employee_id".format(argv[0]))
+        exit(1)
 
-    employee_id = sys.argv[1]
+    employee_id = argv[1]
 
-    # Fetching user data
-    user_response = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
+    # Fetching employee information
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
+    todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id)
+
+    user_response = requests.get(user_url)
+    todos_response = requests.get(todo_url)
+
     user_data = user_response.json()
-    employee_name = user_data.get('name')
-
-    # Fetching todos data
-    todos_response = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id))
     todos_data = todos_response.json()
 
-    # Calculating progress
-    total_tasks = len(todos_data)
+    # Extracting required info
+    employee_name = user_data.get('name')
     completed_tasks = [task for task in todos_data if task.get('completed')]
-    total_completed_tasks = len(completed_tasks)
+    total_tasks = len(todos_data)
 
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, total_completed_tasks, total_tasks))
+    # Printing results
+    print("Employee {} is done with tasks({}/{}):".format(employee_name,
+                                                           len(completed_tasks),
+                                                           total_tasks))
+
     for task in completed_tasks:
-        print("\t{}".format(task.get('title')))
+        print("\t {}".format(task.get('title')))
