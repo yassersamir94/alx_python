dd757d0e011138feb0c2f7c656adf21114773885
Yasser Samir <yassersamir.94@hotmail.com>
2024-03-11 02:23:20 -0400

    0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,35 +1,22 @@
-#!/usr/bin/python3
-"""
-Checks student output for returning info from REST API
-"""
-
 import requests
 import sys
 
-users_url = "https://jsonplaceholder.typicode.com/users"
-todos_url = "https://jsonplaceholder.typicode.com/todos"
+id = sys.argv[1]
 
+request_user = requests.get('https://jsonplaceholder.typicode.com/users/'+id)
+request_todos = requests.get('https://jsonplaceholder.typicode.com/users/'+id+'/todos')
 
-def check_tasks(id):
-    """ Fetch user name, number of tasks """
+data_user = request_user.json()
+data_todos = request_todos.json()
 
-    resp = requests.get(todos_url).json()
+completed = 0
 
-    filename = 'student_output'
-    count = 0
-    with open(filename, 'r') as f:
-        next(f)
-        for line in f:
-            count += 1
-            # Check if the line starts with a tab followed by a space and ends with a newline
-            if line.startswith('\t ') and line.endswith('\n'):
-                print("Task {} Formatting: OK".format(count))
-            else:
-                print("Task {} Formatting: Incorrect".format(count))
+for i in data_todos:
+    if i.get('completed')==True:
+        completed = completed + 1
 
+print ('Employee {} is done with tasks({}/{}):'.format(data_user.get('name'), completed,len(data_todos)))
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py <student_id>")
-        sys.exit(1)
-    check_tasks(int(sys.argv[1]))
+for item in data_todos:
+    if item.get('completed') == True:
+        print('\t ' + item.get('title'))
