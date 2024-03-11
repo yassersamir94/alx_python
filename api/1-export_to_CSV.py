7f0e540db251ae36080da5f59e3ca2e14196339d
Yasser Samir <yassersamir.94@hotmail.com>
2024-03-11 02:29:11 -0400

    1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,21 +1,28 @@
-#!/usr/bin/python3
-import csv
 import requests
 import sys
+import csv
 
-user_id = str(sys.argv[1])
+id = sys.argv[1]
 
-request_user = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
-request_todos = "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
+request_user = requests.get('https://jsonplaceholder.typicode.com/users/'+id)
+request_todos = requests.get('https://jsonplaceholder.typicode.com/users/'+id+'/todos')
 
-data_user = requests.get(request_user).json()
-data_todos = requests.get(request_todos).json()
+data_user = request_user.json()
+data_todos = request_todos.json()
 
-filename = f"{user_id}.csv"
+completed_tasks = []
 
-with open(filename, "w", newline="") as file:
-    csvwriter = csv.writer(file, quoting=csv.QUOTE_ALL)
-    for task in data_todos:
-        csvwriter.writerow(
-            [user_id, str(data_user["username"]), task["completed"], task["title"]]
-        )
+for item in data_todos:
+    if item.get('completed') == True:
+        completed_tasks.append(item)
+
+csv_filename = f"{id}.csv"
+
+with open(csv_filename, 'w', newline='') as csvfile:
+    csv_writer = csv.writer(csvfile)
+    csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+    
+    for task in completed_tasks:
+        csv_writer.writerow([id, data_user.get('name'), str(task.get('completed')), task.get('title')])
+
+print(f"Data exported to {csv_filename}")
