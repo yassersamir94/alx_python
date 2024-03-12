2f75231d416ee762210c15e1f2e8c13b2b897c0f
Yasser Samir <yassersamir.94@hotmail.com>
2024-03-11 22:06:57 -0400

    2

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -2,46 +2,20 @@ import json
 import requests
 import sys
 
-def export_to_json(employee_id):
-    """
-    Fetches employee data and exports it to a JSON file.
-
-    Args:
-    - employee_id (int): The ID of the employee whose data is to be exported.
-
-    Returns:
-    - None
-    """
-    # Retrieve user data
-    user_response = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
-    user_data = user_response.json()
-    username = user_data.get('username')
-
-    # Retrieve todos data
-    todos_response = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id))
-    todos_data = todos_response.json()
-
-    # Prepare JSON data
-    json_data = {employee_id: []}
-
-    for task in todos_data:
-        json_data[employee_id].append({
-            'task': task['title'],
-            'completed': task['completed'],
-            'username': username
-        })
-
-    # Write JSON data to file
-    filename = '{}.json'.format(employee_id)
-    with open(filename, 'w') as json_file:
-        json.dump(json_data, json_file)
-
-    print("Data exported to", filename)
-
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 {} <employee_id>".format(sys.argv[0]))
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
         sys.exit(1)
 
     employee_id = sys.argv[1]
-    export_to_json(employee_id)
+
+    user_response = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
+    user_data = user_response.json()
+
+    todo_response = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id))
+    todo_data = todo_response.json()
+
+    user_todo = {employee_id: [{"task": task['title'], "completed": task['completed'], "username": user_data['username']} for task in todo_data]}
+
+    with open('{}.json'.format(employee_id), 'w') as file:
+        json.dump(user_todo, file)
