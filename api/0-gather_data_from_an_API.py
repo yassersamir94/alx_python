c9792f10aeefb66e4e7fb94c8b8d31ac2bc4fe1b
Yasser Samir <yassersamir.94@hotmail.com>
2024-03-11 21:37:34 -0400

    0, 1

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,39 +1,22 @@
-#!/usr/bin/python3
-"""
-Script that, using a REST API, for a given employee ID,
-returns information about his/her TODO list progress.
-"""
-
 import requests
-from sys import argv
+import sys
 
+id = sys.argv[1]
 
-if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: {} employee_id".format(argv[0]))
-        exit(1)
+request_user = requests.get('https://jsonplaceholder.typicode.com/users/'+id)
+request_todos = requests.get('https://jsonplaceholder.typicode.com/users/'+id+'/todos')
 
-    employee_id = argv[1]
+data_user = request_user.json()
+data_todos = request_todos.json()
 
-    # Fetching employee information
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
-    todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id)
+completed = 0
 
-    user_response = requests.get(user_url)
-    todos_response = requests.get(todo_url)
+for i in data_todos:
+    if i.get('completed')==True:
+        completed = completed + 1
 
-    user_data = user_response.json()
-    todos_data = todos_response.json()
+print ('Employee {} is done with tasks({}/{}):'.format(data_user.get('name'), completed,len(data_todos)))
 
-    # Extracting required info
-    employee_name = user_data.get('name')
-    completed_tasks = [task for task in todos_data if task.get('completed')]
-    total_tasks = len(todos_data)
-
-    # Printing results
-    print("Employee {} is done with tasks({}/{}):".format(employee_name,
-                                                           len(completed_tasks),
-                                                           total_tasks))
-
-    for task in completed_tasks:
-        print("\t {}".format(task.get('title')))
+for item in data_todos:
+    if item.get('completed') == True:
+        print('\t ' + item.get('title'))
--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
-"""
-Script that extends Task 0 to export data in CSV format.
-"""
-
 import csv
 import requests
 import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} employee_id".format(sys.argv[0]))
-        sys.exit(1)
+user_id = str(sys.argv[1])
 
-    employee_id = sys.argv[1]
+request_user = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+request_todos = "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
 
-    user_response = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
-    user_data = user_response.json()
-    employee_name = user_data.get('name')
+data_user = requests.get(request_user).json()
+data_todos = requests.get(request_todos).json()
 
-    todos_response = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id))
-    todos_data = todos_response.json()
+filename = f"{user_id}.csv"
 
-    filename = '{}.csv'.format(employee_id)
-
-    with open(filename, mode='w', newline='') as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        for task in todos_data:
-            writer.writerow([employee_id, employee_name, task.get('completed'), task.get('title')])
+with open(filename, "w", newline="") as file:
+    csvwriter = csv.writer(file, quoting=csv.QUOTE_ALL)
+    for task in data_todos:
+        csvwriter.writerow(
+            [user_id, str(data_user["username"]), task["completed"], task["title"]]
+        )
