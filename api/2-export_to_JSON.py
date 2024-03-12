3d28af52aa22badb9cbdddca0ea67170b332388c
Yasser Samir <yassersamir.94@hotmail.com>
2024-03-11 21:58:00 -0400

    2

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,36 +1,36 @@
-"""
-    This script gathers employee todo data and stores it in JSON format
-"""
-
 import json
 import requests
 import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 export_to_JSON.py <employee_id>")
+        print("Usage: python3 {} <employee_id>".format(sys.argv[0]))
         sys.exit(1)
 
     employee_id = sys.argv[1]
 
+    # Retrieve user data
     user_response = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
     user_data = user_response.json()
-    employee_name = user_data.get('name')
+    username = user_data.get('username')
 
+    # Retrieve todos data
     todos_response = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id))
     todos_data = todos_response.json()
 
-    completed_tasks = []
-    for task in todos_data:
-        task_dict = {
-            "task": task.get('title'),
-            "completed": task.get('completed'),
-            "username": employee_name
-        }
-        completed_tasks.append(task_dict)
+    # Prepare JSON data
+    json_data = {employee_id: []}
 
+    for task in todos_data:
+        json_data[employee_id].append({
+            'task': task['title'],
+            'completed': task['completed'],
+            'username': username
+        })
+
+    # Write JSON data to file
     filename = '{}.json'.format(employee_id)
     with open(filename, 'w') as json_file:
-        json.dump({employee_id: completed_tasks}, json_file, indent=4)
+        json.dump(json_data, json_file)
 
     print("Data exported to", filename)
