772b667511154078b8eaa54415cc404bc220e3af
Yasser Samir <yassersamir.94@hotmail.com>
2024-03-11 21:32:25 -0400

    0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,39 +1,28 @@
 #!/usr/bin/python3
 """
-Script that, using a REST API, for a given employee ID,
-returns information about his/her TODO list progress.
+Script that gathers data from an API.
 """
 
 import requests
-from sys import argv
-
+import sys
 
 if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: {} employee_id".format(argv[0]))
-        exit(1)
+    if len(sys.argv) != 2:
+        print("Usage: {} employee_id".format(sys.argv[0]))
+        sys.exit(1)
 
-    employee_id = argv[1]
+    employee_id = sys.argv[1]
 
-    # Fetching employee information
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
-    todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id)
-
-    user_response = requests.get(user_url)
-    todos_response = requests.get(todo_url)
-
+    user_response = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
     user_data = user_response.json()
+    employee_name = user_data.get('name')
+
+    todos_response = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id))
     todos_data = todos_response.json()
 
-    # Extracting required info
-    employee_name = user_data.get('name')
-    completed_tasks = [task for task in todos_data if task.get('completed')]
     total_tasks = len(todos_data)
+    completed_tasks = [task for task in todos_data if task.get('completed')]
 
-    # Printing results
-    print("Employee {} is done with tasks({}/{}):".format(employee_name,
-                                                           len(completed_tasks),
-                                                           total_tasks))
-
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, len(completed_tasks), total_tasks))
     for task in completed_tasks:
-        print("\t {}".format(task.get('title')))
+        print("\t{}".format(task.get('title')))
