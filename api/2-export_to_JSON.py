f73b76735c3cc5d7ff0ee8ed473f361ab6f9d22a
Yasser Samir <yassersamir.94@hotmail.com>
2024-03-11 21:55:18 -0400

    2

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -2,9 +2,9 @@
     This script gathers employee todo data and stores it in JSON format
 """
 
+import json
 import requests
 import sys
-import json
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
