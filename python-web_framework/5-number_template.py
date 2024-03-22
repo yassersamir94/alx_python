865364c5c7ab1c1e01ad6de7945e2df77e7824cb
Yasser Samir <yassersamir.94@hotmail.com>
2024-03-22 18:27:28 -0400

    0

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -1,50 +1,49 @@
 #!/usr/bin/python3
-"""Importing Flask from flask"""
-from flask import Flask, render_template
-
+"""
+This is module 5-number_template.
+It starts a minimal Flask apllication.
+Run it with python3 -m 5-number_template or ./5-number_template
+"""
+from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
-    """Display 'Hello HBNB!'"""
+    """flask hello world"""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    """Display 'HBNB'"""
+@app.route('/hbnb')
+def hbnb():
+    """add a path to the url"""
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def display_c(text):
-    """Display 'C ' followed by the value of the text variable
-    (replace underscore _ symbols with a space)
-    """
-    return 'C {}'.format(text.replace('_', ' '))
+@app.route('/c/<text>')
+def c_text(text):
+    """make a simple variable rule"""
+    return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def display_python(text):
-    """Display 'Python ' followed by the value of the text variable
-    (replace underscore _ symbols with a space)
-    """
-    return 'Python {}'.format(text.replace('_', ' '))
+@app.route('/python/', defaults={'text': "is cool"})
+@app.route('/python/<text>')
+def python_text(text):
+    """give a rule a default value"""
+    return "Python {}".format(text.replace("_", " "))
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def is_number(n):
-    """Display '<n> is a number' only if n is an integer"""
-    return '{} is a number'.format(n)
+@app.route('/number/<int:n>')
+def number_route(n):
+    """make a rule only take a number"""
+    return "{:d} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """Display a HTML page only if n is an integer: H1 tag: 'Number: n'
-    """
-    return render_template('5-number.html', number=n)
+@app.route('/number_template/<int:number>')
+def number_template(number):
+    """create an html page with a rule"""
+    return render_template('5-number.html', number=number)
 
 
 if __name__ == "__main__":
